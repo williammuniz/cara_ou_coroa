9007c2b4ba8a014236bd6c30cd03931def19241c
William Muniz <william.muz@gmail.com>
2022-05-05 19:23:35 -0300

    first commit

---
+++ lib/Jogar.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'Resultado.dart';
+
+class Jogar extends StatefulWidget {
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado() {
+    var itens = ["cara", "coroa"];
+    int numero = Random().nextInt(2);
+    var resultado = itens[numero];
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+        builder: (context) => Resultado(resultado)
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
